25519279b0a6889818e4e738af3687110e78ea9e
Siddharth <sidhbaha@gmail.com>
2018-12-09 23:11:49 +0530

    final changes

--- ticTacToe/board.js
+++ ticTacToe/board.js
@@ -5,12 +5,34 @@ function Board()
 	this.player2 = new Player("Player O","O");
 	
 	this.turn = 0;
-	
-	this.nextTurn = function ()
+	this.gameWon = false;
+		
+	Board.prototype.nextTurn = function ()
 	{
 		this.turn++;
 		
-		return this.turn%2==0?this.player2:this.player1;
+		this.whosTurn =  this.turn%2==0?this.player2:this.player1;
+		
+		return this.whosTurn;
 	}
+	
+	Board.prototype.setMarker = function(rowIndex,colIndex,markerData)
+	{
+		this.data[rowIndex][colIndex] = markerData;
+		
+	}
+	
+	Board.prototype.getMarker = function(rowIndex,colIndex)
+	{
+		
+		return this.data[rowIndex][colIndex];
+	}
+	
+	Board.prototype.checkGameWon = function()
+	{
+		this.gameWon =  utils.checkColumns.call(this)||utils.checkRows.call(this)||utils.checkDownDiagonal.call(this)||utils.checkUpDiagonal.call(this);
+		
+	}
+	
 	
 }
--- ticTacToe/displayController.js
+++ ticTacToe/displayController.js
@@ -5,19 +5,51 @@ var displayController = (function(){
 	
 		(function createBoard()
 		{
-			board = new Board();
-			root = document.getElementById("root");
-			boardData = board.data;
+			
+			var gameOver = false;			
+			var board = new Board();
+			var boardDiv = document.getElementById("board");
+			var boardData = board.data;
+			var gameDiv = document.createElement("div");   // game container
+			boardDiv.appendChild(gameDiv);
+			gameDiv.classList.add("nestedGrid");
 			boardData.forEach(function(row,rowIndex)
 			{
 				// for each row create the divs
 				row.forEach(function(cell,cellIndex)
 					{
 						// create that cell
-						cell = document.createElement("div");
-						cell.addEventListener("click",onCellClick.bind(null,cell,board));
-						cell.textContent="&nbsp;";
-						root.appendChild(cell);
+						
+						var cell = document.createElement("div");
+						cell.classList.add("cell","position");
+						cell.addEventListener("click",function onCellClick(){
+							if(!board.gameWon){
+							var currentPlayer = board.nextTurn();
+							cell.textContent = currentPlayer.marker;
+							board.setMarker(rowIndex,cellIndex,currentPlayer.marker);
+							cell.removeEventListener("click",onCellClick);
+							board.checkGameWon();
+							if(board.gameWon)
+							{
+								gameOver = `${currentPlayer.name} won the game`;
+								
+							}
+							if(board.turn>=9)
+							{
+								gameOver = "Tie Game";
+							}
+							}
+							
+							if(gameOver)
+							{
+								var gameResult = document.createElement("div");
+								gameResult.classList.add("gameResult");
+								gameResult.textContent = gameOver;
+								boardDiv.appendChild(gameResult);
+							}
+						});
+						
+						gameDiv.appendChild(cell);
 					}
 				)
 			});
@@ -25,9 +57,5 @@ var displayController = (function(){
 		})();
 	
 	
-	function onCellClick(cell,board)
-	{
-		// write x or o based on the turn
-		cell.textContent = board.nextTurn().marker;
-	}
+	
 })();
